ff44ff43aa2e0f0b2b37d00cdbd4ec45aeaee436
Rares Matei <matei.rar@gmail.com>
2022-07-21 20:24:41 +0100

    upgrade to latest nx

--- test-fail.sh
+++ test-fail.sh
@@ -1,8 +1,33 @@
-   pids=()
-           npm run nx affected -- --target=60s-delay-okay --base=origin/main &
-           pids+=($!)
-           npm run nx affected -- --target=20s-delay-fail --base=origin/main &
-           pids+=($!)
-           for pid in "${pids[@]}"; do
-             wait "$pid"
-           done
+#pids=()
+#npm run nx affected -- --target=test --base=origin/main --skip-nx-cache &
+#pids+=($!)
+#npm run nx affected -- --target=60s-delay-fail --base=origin/main &
+#pids+=($!)
+#npm run nx affected -- --target=20s-delay-fail --base=origin/main &
+#pids+=($!)
+#for pid in "${pids[@]}"; do
+#wait "$pid"
+#done
+
+FAIL=0
+
+echo "starting"
+
+npm run nx affected -- --target=test --base=origin/main --skip-nx-cache &
+npm run nx affected -- --target=60s-delay-fail --base=origin/main &
+npm run nx affected -- --target=20s-delay-fail --base=origin/main &
+
+for job in `jobs -p`
+do
+echo $job
+    wait $job || let "FAIL+=1"
+done
+
+echo $FAIL
+
+if [ "$FAIL" == "0" ];
+then
+echo "YAY!"
+else
+echo "FAIL! ($FAIL)"
+fi
