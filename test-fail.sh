94b996b460e53836e4f9d56e55013f8e94e7a441
Rares Matei <matei.rar@gmail.com>
2022-07-22 10:09:44 +0100

    upgrade to latest nx

--- test-fail.sh
+++ test-fail.sh
@@ -1,33 +1,33 @@
-#pids=()
-#npm run nx affected -- --target=test --base=origin/main --skip-nx-cache &
-#pids+=($!)
-#npm run nx affected -- --target=60s-delay-fail --base=origin/main &
-#pids+=($!)
-#npm run nx affected -- --target=20s-delay-fail --base=origin/main &
-#pids+=($!)
-#for pid in "${pids[@]}"; do
-#wait "$pid"
-#done
-
-FAIL=0
-
-echo "starting"
-
+pids=()
 npm run nx affected -- --target=test --base=origin/main --skip-nx-cache &
+pids+=($!)
 npm run nx affected -- --target=60s-delay-fail --base=origin/main &
+pids+=($!)
 npm run nx affected -- --target=20s-delay-fail --base=origin/main &
-
-for job in `jobs -p`
-do
-echo $job
-    wait $job || let "FAIL+=1"
+pids+=($!)
+for pid in "${pids[@]}"; do
+wait "$pid"
 done
 
-echo $FAIL
-
-if [ "$FAIL" == "0" ];
-then
-echo "YAY!"
-else
-echo "FAIL! ($FAIL)"
-fi
+#FAIL=0
+#
+#echo "starting"
+#
+#npm run nx affected -- --target=test --base=origin/main --skip-nx-cache &
+#npm run nx affected -- --target=60s-delay-fail --base=origin/main &
+#npm run nx affected -- --target=20s-delay-fail --base=origin/main &
+#
+#for job in `jobs -p`
+#do
+#echo $job
+#    wait $job || let "FAIL+=1"
+#done
+#
+#echo $FAIL
+#
+#if [ "$FAIL" == "0" ];
+#then
+#echo "YAY!"
+#else
+#echo "FAIL! ($FAIL)"
+#fi
